eecd043caa6f5bd44db2c55ba01cf694858ebcfe
PeterDigger <18097872+PeterDigger@users.noreply.github.com>
2021-12-17 23:32:29 +0800

    updated shuitsdss

---
+++ include/test.h
@@ -0,0 +1,9 @@
+#ifndef H_test
+#define H_test
+
+void hello() 
+{
+    std::cout << "Hellow";
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include<iostream>
+#include <iostream>
+#include "include/test.h"
+
 using namespace std;
 
 int main()
 {
+    hello();
     return 0;
 }
