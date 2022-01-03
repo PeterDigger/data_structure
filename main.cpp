c85197c7c2a0da680071089baf6a60655030ea93
PeterDigger <18097872+PeterDigger@users.noreply.github.com>
2022-01-03 23:22:50 +0800

    Add comments and todo's
    1. Removed shenanigans things
    2. Added bunch of comments for future revisions

--- main.cpp
+++ main.cpp
@@ -1,16 +1,64 @@
 #include <iostream>
+// TODO add wxwidget library
+// #include <wx/wxio.h>
 #include "include/test.h"
+
+/*
+███╗   ██╗██████╗███████████████╗       
+████╗  ████╔═══██╚══██╔══██╔════╝    ██╗
+██╔██╗ ████║   ██║  ██║  █████╗      ╚═╝
+██║╚██╗████║   ██║  ██║  ██╔══╝      ██╗
+██║ ╚████╚██████╔╝  ██║  ███████╗    ╚═╝
+╚═╝  ╚═══╝╚═════╝   ╚═╝  ╚══════╝
+
+1. searching for records
+2. sort the records according i.e. states, matric no, year of study, etc.
+3. add/delete records
+4. shuffle the records so that the list of mentees can be assigned randomly to the lecturers
+*/
+
+/* 
+████████╗██████╗██████╗ ██████╗        
+╚══██╔══██╔═══████╔══████╔═══██╗    ██╗
+   ██║  ██║   ████║  ████║   ██║    ╚═╝
+   ██║  ██║   ████║  ████║   ██║    ██╗
+   ██║  ╚██████╔██████╔╚██████╔╝    ╚═╝
+   ╚═╝   ╚═════╝╚═════╝ ╚═════╝        
+
+[CHONG]: Please setup the wxWidget by 4th Jan 22.
+
+1. Adding library wxWidget for GUI
+2. Make sure to have the basic features such as insert, delete, sort, searching, shuffle the records.
+3. Present it in the GUI window
+    a. tabs navigation?
+    b. How to store the records? in what form?
+*/
 
 using namespace std;
 
-int main()
+void insert(/*Take in dynamic array to find*/){
+
+}
+
+void deleteRecord(/*Take in dynamic array to delete*/){
+    
+}
+
+void sort(/*Take in dynamic array to sort*/){
+
+}
+
+int main() //TODO this section will be more on calling GUI library and place  
 {
-    hello();
-    cout<<"hello";
+    //TODO insert the records
 
-    cout << "Hey Guys... KT handsome here";
+    //TODO delete the records
 
-    cout << "hi Chongo!";
+    //TODO sort the records
+    
+    //TODO searching for records
+
+    //TODO shuffle the records
 
     return 0;
 }
